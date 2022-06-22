a2becf636627e23e9f0bf32b04cf3d2b9328b51e
Minji Kim <rlaalswl0320@naver.com>
2022-06-22 10:22:37 +0900

    Feat: 02-11. 캡슐화

---
+++ Chapter2/src/ch11/ReportTest.java
@@ -0,0 +1,12 @@
+package ch11;
+
+public class ReportTest {
+
+    public static void main(String[] args) {
+
+        MakeReport report = new MakeReport();
+        String builder = report.getReport();
+
+        System.out.println(builder);
+    }
+}

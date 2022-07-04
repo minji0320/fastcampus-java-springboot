eb5690186f8deca2db36c06f265f5fc3edbc517a
minji0320 <rlaalswl0320@naver.com>
2022-07-04 10:21:42 +0900

    Feat: 03-09. 추상 클래스의 의미와 구현하는 방법

---
+++ Chapter3/src/ch09/ComputerTest.java
@@ -0,0 +1,15 @@
+package ch09;
+
+public class ComputerTest {
+
+    public static void main(String[] args) {
+
+        DeskTop deskTop = new DeskTop();
+//        Computer computer = new Computer();
+
+        deskTop.display();
+        deskTop.typing();
+        deskTop.turnOff();
+        deskTop.turnOn();
+    }
+}

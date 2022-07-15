46668e92ea9486b9a4adf70516c0520a04b42c69
minji0320 <rlaalswl0320@naver.com>
2022-07-15 10:43:01 +0900

    Feat: 06-05~09. 강의 수강
    - 05 스트림(Stream)
    - 06 객체지향 프로그래밍 방식과 람다식 구현 방식의 비교
    - 07 연산 수행에 대한 구현을 할 수 있는 reduce() 메서드
    - 08 스트림을 활용하여 패키지 여행 비용 계산하기
    - 09 예외처리는 왜 해야 하나 자바에서 제공되는 클래스들

---
+++ Chapter06/src/ch06/StringConcat.java
@@ -0,0 +1,6 @@
+package ch06;
+
+public interface StringConcat {
+
+    public void makeString(String s1, String s2);
+}
---
+++ Chapter06/src/ch06/StringConcatTest.java
@@ -0,0 +1,16 @@
+package ch06;
+
+public class StringConcatTest {
+
+    public static void main(String[] args) {
+
+        String s1 = "Hello";
+        String s2 = "World";
+
+        StringConcatImpl strImpl = new StringConcatImpl();
+        strImpl.makeString(s1, s2);
+
+        StringConcat concat = (s, v) -> System.out.println(s + "," + v);
+        concat.makeString(s1, s2);
+    }
+}
